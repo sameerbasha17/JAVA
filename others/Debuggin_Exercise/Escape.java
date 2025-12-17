fe5ff97b3c71d566b0193fc9b97fc0f4f02dfc1f
sameerbasha17 <sksameerbasha703@gmail.com>
2025-12-17 22:49:04 +0530

    Added from Debugging Exercises from Java-One-Step-Ahead book

---
+++ others/Debuggin_Exercise/Escape.java
@@ -0,0 +1,22 @@
+/*
+TASK: Debug the following program and run it.
+class Escape
+{public static void main(String Str[])
+    {int n = 20; m = 4, a = 10, b = 5;
+     System.out.print("Value of a = " +a+ "\t b = " +b+"/n")
+    System.out.println(m + "\t" + n + "\t" + n*m);
+    System.out.println("\'Value of b\' = " + b);
+    System.out.println("\'Value of n\' + " + n);
+}} */
+public class Escape 
+{
+    public static void main(String str[])
+    {  
+        int n = 20, m = 4, a = 10, b = 5;
+
+        System.out.print("Value of a = " + a + "\t b = " + b +"\n");
+        System.out.println(m + "\t" + n + "\t" + n*m);
+        System.out.println("\'Value of b\' = " + b);
+        System.out.println("\'Value of n\' = " + n);
+    }
+}
