ae6512564f536d7750a729e63792517f1350900b
sameerbasha17 <sksameerbasha703@gmail.com>
2025-12-18 19:00:15 +0530

    Basic concepts

---
+++ Basic/MyFirst.java
@@ -0,0 +1,40 @@
+// By Default the java.lang package is imported
+// Every Java program must have a class
+// The file name must be same as the class name
+// The main method is the entry point of any Java program
+
+public class MyFirst{                                  // class name is MyFirst
+    public static void main(String [] args){           // main method     
+        System.out.println("Hello , Sameer!");        // printing a string to the console   
+    }
+}
+
+
+// POINTS TO REMEMBER :
+
+/*  
+ 1.Class name must start with a capital letter
+ 2. Class names should be nouns, in mixed case with the first letter of each internal word capitalized
+ 3. Class names should be unique
+ 4. If a class is public, the file name must be the same as the class name
+ 5. If a class is not public, it can have any name
+ 6. A Java program can have multiple classes, but only one public class */
+
+
+/*  
+☆ public is an access modifier
+☆ static means that the method belongs to the class and not to any specific instance of the class
+☆ void means that the method does not return any value
+☆ String[] args is an array of strings that stores command line arguments
+☆ main is the name of the method
+☆ Every Java application must contain a main method
+
+➤ println is a method of the PrintStream class
+➤ System is a class in the java.lang package
+➤ out is a static member of the System class and is an instance of PrintStream class
+➤ print() method is used to print text to the console
+➤ ln in println stands for new line
+➤ Every statement in Java must end with a semicolon (;)
+➤ Java is case sensitive
+*/
+    
