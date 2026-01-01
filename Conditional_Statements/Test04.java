72447116c1b19218b348db957f9b163dc8cdd66b
sameerbasha17 <sksameerbasha703@gmail.com>
2026-01-01 13:38:17 +0530

    Added Test04

---
+++ Conditional_Statements/Test04.java
@@ -0,0 +1,18 @@
+
+
+public class Test04 
+{
+    public static void main(String [] args)
+    {
+        int a = 10;
+
+        if(a++ == 10)
+        {
+            System.out.println(a);
+        }
+        else{
+            System.out.println("hello");
+        }
+        System.out.println(a);
+    }
+}
