00f42a5d2b95cfcf35c42aab269da4227970c0a9
sameerbasha17 <sksameerbasha703@gmail.com>
2026-03-06 19:41:15 +0530

    Added Pattern 12

---
+++ Striver_Sheet_A2Z/Patterns/Pattern12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Pattern12
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // input
+        System.out.print("Enter number of lines : ");
+        int n = sc.nextInt();
+       
+        for(int i = 1; i <= n ; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for(int j = 1; j <= (n * 2 - i * 2); j ++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = i; j >= 1; j --)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
